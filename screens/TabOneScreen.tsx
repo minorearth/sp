93bc810263b72a0611198dd912ad0d1f773f0822
RomanInformatika <officepm@yandex.ru>
2022-11-13 01:24:26 +0300

    Перенес код из Аниного проекта.Добавил функцию фильтрации по дате

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,12 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,FlatList, ActivityIndicator,Text,View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+// import { Text,} from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import { useSelector } from 'react-redux';
+import{ISOdateParse,filterByToday} from '../utils'
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  
   let [isLoading, setIsLoading] = useState(true);
   let [error, setError] = useState();
   let [response, setResponse] = useState();
@@ -21,8 +23,8 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       .then(res => res.json())
       .then(
         (result) => {
-          console.log(result.value)
-          setResponse(result);
+          // console.log(result)
+          setResponse(filterByToday(result));
           setIsLoading(false);
         },
         (error) => {
@@ -31,24 +33,85 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         }
       )
   }, []);
-  const count = useSelector((state) => state.userdata.value)
+
+  
+
+  return (
+    // <View style={styles.container}>
+    <View style={styles.container}>
+      {/* {getContent()} */}
+      {!isLoading && <Text style={styles.library}>{response.value[0].Address}</Text>}
+      
+      <FlatList
+        data={response?.value}
+        renderItem={(item) => {
+          return <View style={styles.box}>
+            <View style={styles.what1}><Text style={styles.what}>{item.item.Group[0].Title}</Text></View>
+            <View style={styles.where1}><Text style={styles.where}>{item.item.Address}</Text><View/></View>
+            <View style={styles.who1}><Text style={styles.who}>{item.item.MainMan.Title}</Text></View>
+            <View style={styles.data1}><Text style={styles.data}>{ISOdateParse(item.item.DateStart)}, {item.item.Time}</Text></View>
+
+            <Text>  </Text>
+            <Text>   </Text>
+          </View>
+        }}
+        keyExtractor={(item) => item.id}
+        vertical
+      />
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#00FFFF',
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#8888FF'
+    justifyContent: 'center'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  box: {
+    backgroundColor: '#CC66FF',
+    borderRadius: 20,
+    marginTop: 20
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-    backgroundColor: '#8888FF'
+  what: {
+    fontSize: 18,
+  },
+  where: {
+    fontSize: 18,
+  },
+  who: {
+    fontSize: 18,
+  },
+  data: {
+    fontSize: 18,
+  },
+  library: {
+    fontSize: 25,
+    borderRadius: 10,
+  },
+  what1: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 10
+  },
+  where1: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 10
+  },
+  who1: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 10
+  },
+  data1: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 10
   },
 });
---
+++ utils.js
@@ -0,0 +1,32 @@
+const SintNow= '2022-09-28'
+export const ISOdateParse = (IsoDate) => {
+    var now = new Date(IsoDate);
+    const timecur = now.toISOString().substring(0, 10)
+    return timecur
+    // return now>SintNow?timecur:'no'
+ 
+}
+
+export const filterByToday = (events) => {
+    if (events==undefined){
+        return {}
+    }
+    const res = {}
+    res["value"] =[]
+    // const data3 = {"value":[{ "DateStart": '2022-09-29T21:00:00Z' }, { "DateStart": '2022-09-29T21:00:00Z' }]}
+    const data3 = events
+    const data2=data3.value
+    for (var event in data2) {
+        var eventDate = new Date(data2[event].DateStart);
+        
+        if ( eventDate.toISOString().substring(0, 10)== SintNow) {
+            res["value"] = [...res["value"],data2[event]]
+        }
+    }
+    // console.log(events.value.slice(0,1))
+    // console.log(res.value.slice(0,1))
+    return res
+
+
+}
+filterByToday()
